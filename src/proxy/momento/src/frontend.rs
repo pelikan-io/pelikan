20e40e31fac16966c2df6e99ecd1b31956c03fa9
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-02-06 15:00:53 -0500

    feat: Implement resp LTRIM command (#55)

--- src/proxy/momento/src/frontend.rs
+++ src/proxy/momento/src/frontend.rs
@@ -152,6 +152,9 @@ pub(crate) async fn handle_resp_client(
                 resp::Request::ListPushBack(r) => {
                     resp::rpush(&mut client, &cache_name, &mut response_buf, r).await?
                 }
+                resp::Request::ListTrim(r) => {
+                    resp::ltrim(&mut client, &cache_name, &mut response_buf, r).await?
+                }
                 resp::Request::Set(r) => {
                     resp::set(&mut client, &cache_name, &mut socket, &r).await?
                 }
