e10c48cde2e5be7fd99335fb87f6568bc5b8334c
Brian Martin <brian@pelikan.io>
2023-02-07 09:17:04 -0800

    fix: remove debug output and add resp fuzzer (#56)
    Removes some debug output that we don't want which was discovered
    while adding the fuzzer for the resp protocol.

---
+++ src/protocol/resp/fuzz/fuzz_targets/resp.rs
@@ -0,0 +1,22 @@
+// Copyright 2021 Twitter, Inc.
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+//! A fuzz target which makes sure that the `RESP` protocol implementation will
+//! handle arbitrary data without panicking.
+
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use protocol_resp::*;
+use protocol_common::Parse;
+
+// TODO(bmartin): we should be able to do some validation like we do in the
+// memcache protocol fuzzing. For now, this just makes sure the parser will
+// not panic on unanticipated inputs.
+
+fuzz_target!(|data: &[u8]| {
+    let parser = RequestParser::new();
+
+    let _ = parser.parse(data);
+});
