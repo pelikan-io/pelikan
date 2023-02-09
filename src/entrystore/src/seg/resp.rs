0cd8530332e075b294d3213d5566eef59693331b
Dermot Haughey <hderms@gmail.com>
2023-02-09 10:46:29 -0600

    add del operation

--- src/entrystore/src/seg/resp.rs
+++ src/entrystore/src/seg/resp.rs
@@ -15,6 +15,7 @@ use std::time::Duration;
 impl Execute<Request, Response> for Seg {
     fn execute(&mut self, request: &Request) -> Response {
         match request {
+            Request::Del(del) => self.del(del),
             Request::Get(get) => self.get(get),
             Request::Set(set) => self.set(set),
             _ => Response::error("not supported"),
@@ -23,6 +24,16 @@ impl Execute<Request, Response> for Seg {
 }
 
 impl Storage for Seg {
+    fn del(&mut self, delete: &Del) -> Response {
+        let count: usize = delete
+            .keys()
+            .iter()
+            .filter(|key| self.data.delete(key))
+            .count();
+        // according to https://stackoverflow.com/questions/42911672/is-there-any-limit-on-the-number-of-arguments-that-redis-commands-such-as-zadd-o#:~:text=The%20maximum%20number%20of%20arguments,long%20meaning%20up%20to%202%2C147%2C483%2C647.
+        // the max number of arguments to a Redis command is the max value of an i32, so this cast should be safe
+        Response::integer(count as i64)
+    }
     fn get(&mut self, get: &Get) -> Response {
         if let Some(item) = self.data.get(get.key()) {
             match item.value() {
