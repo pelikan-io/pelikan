b0896a7bc8fcce96e4947fff30eb6ca6039334a3
Brian Martin <brayniac@gmail.com>
2024-09-18 14:44:53 -0700

    address review feedback

--- src/server/pingserver/src/main.rs
+++ src/server/pingserver/src/main.rs
@@ -6,10 +6,11 @@ use config::{Config, Engine};
 use entrystore::Noop;
 use logger::{configure_logging, Drain};
 use protocol_ping::{Request, RequestParser, Response};
-use server::ProcessBuilder;
+use server::{PERCENTILES, ProcessBuilder};
 
 use backtrace::Backtrace;
 use clap::{Arg, Command};
+use metriken::*;
 
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
@@ -33,13 +34,65 @@ fn main() {
     // parse command line options
     let matches = Command::new(env!("CARGO_BIN_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
+        .long_about(
+            "A rust implementation of, arguably, the most over-engineered ping \
+            server.\n\n\
+            The purpose is to demonstrate how to create an otherwise minimal \
+            service with the libraries and modules provied by Pelikan, which \
+            meets stringent requirements on latencies, observability, \
+            configurability, and other valuable traits in a typical production \
+            environment.",
+        )
         .arg(
             Arg::new("CONFIG")
                 .help("Server configuration file")
                 .action(clap::ArgAction::Set)
                 .index(1),
         )
+        .arg(
+            Arg::new("stats")
+                .short('s')
+                .long("stats")
+                .help("List all metrics in stats")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
+
+    if matches.get_flag("stats") {
+        println!("{:<31} {:<15} DESCRIPTION", "NAME", "TYPE");
+
+        let mut metrics = Vec::new();
+
+        for metric in &metriken::metrics() {
+            let any = match metric.as_any() {
+                Some(any) => any,
+                None => {
+                    continue;
+                }
+            };
+
+            if any.downcast_ref::<Counter>().is_some() {
+                metrics.push(format!("{:<31} counter", metric.name()));
+            } else if any.downcast_ref::<Gauge>().is_some() {
+                metrics.push(format!("{:<31} gauge", metric.name()));
+            } else if any.downcast_ref::<AtomicHistogram>().is_some()
+                || any.downcast_ref::<RwLockHistogram>().is_some()
+            {
+                for (label, _) in PERCENTILES {
+                    let name = format!("{}_{}", metric.name(), label);
+                    metrics.push(format!("{name:<31} percentile"));
+                }
+            } else {
+                continue;
+            }
+        }
+
+        metrics.sort();
+        for metric in metrics {
+            println!("{metric}");
+        }
+        std::process::exit(0);
+    }
 
     // load config from file
     let config = if let Some(file) = matches.get_one::<String>("CONFIG") {
