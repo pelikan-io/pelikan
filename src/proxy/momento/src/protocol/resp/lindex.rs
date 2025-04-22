cb5ef421916c4c020addf1427a16e97f78c09c8f
Michael Landis <michael@momentohq.com>
2025-04-22 15:49:47 -0700

    fix: redis/lindex

--- src/proxy/momento/src/protocol/resp/lindex.rs
+++ src/proxy/momento/src/protocol/resp/lindex.rs
@@ -5,6 +5,7 @@
 use std::io::Write;
 use std::time::Duration;
 
+use momento::cache::ListFetchResponse;
 use momento::CacheClient;
 use protocol_resp::{ListIndex, LINDEX, LINDEX_EX, LINDEX_HIT, LINDEX_MISS};
 
@@ -42,50 +43,53 @@ pub async fn lindex(
             }
         };
 
-        if let Some(entry) = entry {
-            let list = entry.value();
-            let index: Option<usize> = match req.index() {
-                index @ 0.. => index.try_into().ok(),
-                index => (-index)
-                    .try_into()
-                    .map(|index: usize| list.len() - index)
-                    .ok(),
-            };
+        match entry {
+            ListFetchResponse::Hit { values } => {
+                // TODO need to impl from here
+                let list: Vec<Vec<u8>> = values.try_into()?;
+                let index: Option<usize> = match req.index() {
+                    index @ 0.. => index.try_into().ok(),
+                    index => (-index)
+                        .try_into()
+                        .map(|index: usize| list.len() - index)
+                        .ok(),
+                };
 
-            let status = match index.and_then(|index| list.get(index)).map(|x| &**x) {
-                Some(element) => {
-                    write!(response_buf, "${}\r\n", element.len())?;
-                    response_buf.extend_from_slice(element);
-                    response_buf.extend_from_slice(b"\r\n");
+                let status = match index.and_then(|index| list.get(index)).map(|x| &**x) {
+                    Some(element) => {
+                        write!(response_buf, "${}\r\n", element.len())?;
+                        response_buf.extend_from_slice(element);
+                        response_buf.extend_from_slice(b"\r\n");
 
-                    LINDEX_HIT.increment();
-                    Status::Hit
-                }
-                None => {
-                    write!(response_buf, "$-1\r\n")?;
+                        LINDEX_HIT.increment();
+                        Status::Hit
+                    }
+                    None => {
+                        write!(response_buf, "$-1\r\n")?;
 
-                    LINDEX_MISS.increment();
-                    Status::Miss
-                }
-            };
+                        LINDEX_MISS.increment();
+                        Status::Miss
+                    }
+                };
 
-            let index = format!("{}", req.index());
-            klog_2(&"lindex", &req.key(), &index, status, response_buf.len())
-        } else {
-            write!(response_buf, "$-1\r\n")?;
+                let index = format!("{}", req.index());
+                klog_2(&"lindex", &req.key(), &index, status, response_buf.len())
+            }
+            ListFetchResponse::Miss => {
+                write!(response_buf, "$-1\r\n")?;
 
-            LINDEX_MISS.increment();
+                LINDEX_MISS.increment();
 
-            let index = format!("{}", req.index());
-            klog_2(
-                &"lindex",
-                &req.key(),
-                &index,
-                Status::Miss,
-                response_buf.len(),
-            )
+                let index = format!("{}", req.index());
+                klog_2(
+                    &"lindex",
+                    &req.key(),
+                    &index,
+                    Status::Miss,
+                    response_buf.len(),
+                )
+            }
         }
-
         Ok(())
     })
     .await
