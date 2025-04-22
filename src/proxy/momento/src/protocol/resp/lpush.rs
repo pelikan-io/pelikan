c139c22938dc415edb2393731ae67337fca4f2a4
Michael Landis <michael@momentohq.com>
2025-04-22 16:02:38 -0700

    fix: update redis/lpush

--- src/proxy/momento/src/protocol/resp/lpush.rs
+++ src/proxy/momento/src/protocol/resp/lpush.rs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-use std::io::Write;
-
 use crate::*;
 use protocol_resp::{ListPush, LPUSH, LPUSH_EX};
 
@@ -12,11 +10,11 @@ use super::update_method_metrics;
 pub async fn lpush(
     client: &mut CacheClient,
     cache_name: &str,
-    response_buf: &mut Vec<u8>,
+    _: &mut Vec<u8>,
     req: &ListPush,
 ) -> ProxyResult {
     update_method_metrics(&LPUSH, &LPUSH_EX, async move {
-        let count = timeout(
+        timeout(
             Duration::from_millis(200),
             client.list_concatenate_front(
                 cache_name,
