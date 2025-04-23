1f57e8308fd2e6694ab7e207c23480cdd97d1fa5
Michael Landis <michael@momentohq.com>
2025-04-22 18:59:55 -0700

    fix: update redis/srem

--- src/proxy/momento/src/protocol/resp/srem.rs
+++ src/proxy/momento/src/protocol/resp/srem.rs
@@ -2,11 +2,10 @@
 // Licensed under the Apache License, Version 2.0
 // http://www.apache.org/licenses/LICENSE-2.0
 
-use std::io::Write;
-use std::time::Duration;
-use momento::cache::SetRemoveElementsResponse;
 use momento::CacheClient;
 use protocol_resp::{SetRem, SREM, SREM_EX};
+use std::io::Write;
+use std::time::Duration;
 
 use crate::error::ProxyResult;
 
@@ -21,7 +20,7 @@ pub async fn srem(
     update_method_metrics(&SREM, &SREM_EX, async move {
         let elements = req.members().iter().map(|e| &**e).collect();
 
-        let resp = tokio::time::timeout(
+        tokio::time::timeout(
             Duration::from_millis(200),
             client.set_remove_elements(cache_name, req.key(), elements),
         )
@@ -29,11 +28,7 @@ pub async fn srem(
 
         // Momento doesn't return the info we need here so we pretend that
         // all the elements were removed from the set.
-        write!(
-            response_buf,
-            ":{}\r\n",
-            req.members().len()
-        )?;
+        write!(response_buf, ":{}\r\n", req.members().len())?;
 
         Ok(())
     })
