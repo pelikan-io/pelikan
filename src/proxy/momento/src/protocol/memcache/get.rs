231eb8172ca0b9faeea227c2a7c324bad75497de
Kenny <kenny@momentohq.com>
2025-04-30 16:34:03 -0700

    parallelize batch gets

--- src/proxy/momento/src/protocol/memcache/get.rs
+++ src/proxy/momento/src/protocol/memcache/get.rs
@@ -4,6 +4,7 @@
 
 use crate::klog::{klog_1, Status};
 use crate::{Error, *};
+use futures::StreamExt;
 use momento::cache::GetResponse;
 use protocol_memcache::GET_EX;
 use protocol_memcache::GET_KEY_HIT;
@@ -11,7 +12,7 @@ use protocol_memcache::GET_KEY_MISS;
 use protocol_memcache::{Get, Response, Value};
 
 pub async fn get(
-    client: &mut CacheClient,
+    client: &CacheClient,
     cache_name: &str,
     request: &Get,
     flags: bool,
@@ -33,65 +34,72 @@ pub async fn get(
         }
     };
 
-    let mut values = Vec::new();
-
+    let mut tasks = futures::stream::FuturesOrdered::new();
     for key in keys {
         BACKEND_REQUEST.increment();
 
-        match timeout(Duration::from_millis(200), client.get(cache_name, key)).await {
-            Ok(Ok(response)) => match response {
-                GetResponse::Hit { value } => {
-                    GET_KEY_HIT.increment();
-
-                    let value: Vec<u8> = value.into();
-
-                    if flags && value.len() < 5 {
-                        klog_1(&"get", &key, Status::Miss, 0);
-                    } else if flags {
-                        let flags: u32 =
-                            u32::from_be_bytes([value[0], value[1], value[2], value[3]]);
-                        let value: Vec<u8> = value[4..].into();
-                        let length = value.len();
-
-                        values.push(Value::new(key.as_bytes(), flags, None, &value));
-
-                        klog_1(&"get", &key, Status::Hit, length);
-                    } else {
-                        let length = value.len();
-                        values.push(Value::new(key.as_bytes(), 0, None, &value));
-
-                        klog_1(&"get", &key, Status::Hit, length);
-                    }
-                }
-                GetResponse::Miss => {
-                    GET_KEY_MISS.increment();
-
-                    klog_1(&"get", &key, Status::Miss, 0);
-                }
-            },
-            Ok(Err(e)) => {
-                // we got some error from the momento client
-                // log and incr stats and move on treating it
-                // as a miss
-                error!("backend error for get: {}", e);
-                BACKEND_EX.increment();
-
-                klog_1(&"get", &key, Status::ServerError, 0);
-            }
-            Err(_) => {
-                // we had a timeout, incr stats and move on
-                // treating it as a miss
-                BACKEND_EX.increment();
-                BACKEND_EX_TIMEOUT.increment();
-
-                klog_1(&"get", &key, Status::Timeout, 0);
-            }
-        }
+        tasks.push_back(run_get(client, cache_name, flags, key));
     }
+    let values: Vec<Option<Value>> = tasks.collect().await;
+    let values: Vec<Value> = values.into_iter().filter_map(|v| v).collect();
 
     if !values.is_empty() {
         Ok(Response::values(values.into()))
     } else {
         Ok(Response::not_found(false))
+    }
+}
+
+async fn run_get(client: &CacheClient, cache_name: &str, flags: bool, key: &str) -> Option<Value> {
+    match timeout(Duration::from_millis(200), client.get(cache_name, key)).await {
+        Ok(Ok(response)) => match response {
+            GetResponse::Hit { value } => {
+                GET_KEY_HIT.increment();
+
+                let value: Vec<u8> = value.into();
+
+                if flags && value.len() < 5 {
+                    klog_1(&"get", &key, Status::Miss, 0);
+                    None
+                } else if flags {
+                    let flags: u32 = u32::from_be_bytes([value[0], value[1], value[2], value[3]]);
+                    let value: Vec<u8> = value[4..].into();
+                    let length = value.len();
+
+                    klog_1(&"get", &key, Status::Hit, length);
+                    Some(Value::new(key.as_bytes(), flags, None, &value))
+                } else {
+                    let length = value.len();
+
+                    klog_1(&"get", &key, Status::Hit, length);
+                    Some(Value::new(key.as_bytes(), 0, None, &value))
+                }
+            }
+            GetResponse::Miss => {
+                GET_KEY_MISS.increment();
+
+                klog_1(&"get", &key, Status::Miss, 0);
+                None
+            }
+        },
+        Ok(Err(e)) => {
+            // we got some error from the momento client
+            // log and incr stats and move on treating it
+            // as a miss
+            error!("backend error for get: {}", e);
+            BACKEND_EX.increment();
+
+            klog_1(&"get", &key, Status::ServerError, 0);
+            None
+        }
+        Err(_) => {
+            // we had a timeout, incr stats and move on
+            // treating it as a miss
+            BACKEND_EX.increment();
+            BACKEND_EX_TIMEOUT.increment();
+
+            klog_1(&"get", &key, Status::Timeout, 0);
+            None
+        }
     }
 }
