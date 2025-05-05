c10fcaa5ef708ac8174fbde1444ced45f3699fdf
anitarua <anita@momentohq.com>
2025-05-05 16:23:51 -0700

    fix lint error

--- src/proxy/momento/src/protocol/resp/zrange.rs
+++ src/proxy/momento/src/protocol/resp/zrange.rs
@@ -14,7 +14,9 @@ use crate::error::ProxyResult;
 use crate::klog::{klog_1, Status};
 use crate::ProxyError;
 
-use super::update_method_metrics;
+use super::{
+    parse_score_boundary_as_float, parse_score_boundary_as_integer, update_method_metrics,
+};
 
 pub async fn zrange(
     client: &mut CacheClient,
@@ -34,8 +36,8 @@ pub async fn zrange(
         let response = match *req.range_type() {
             RangeType::ByIndex => {
                 // ByIndex accepts only integers as inclusive start and inclusive stop values
-                let start = parse_as_integer(req.start())?;
-                let stop = parse_as_integer(req.stop())?;
+                let start = parse_score_boundary_as_integer(req.start())?;
+                let stop = parse_score_boundary_as_integer(req.stop())?;
 
                 let order = match req.optional_args().reversed {
                     Some(true) => SortedSetOrder::Descending,
@@ -67,8 +69,8 @@ pub async fn zrange(
             }
             RangeType::ByScore => {
                 // ByScore can accept start/stop values: `(` for exclusive boundary, `+inf`, `-inf`.
-                let (start, exclusive_start) = parse_as_float(req.start())?;
-                let (stop, exclusive_stop) = parse_as_float(req.stop())?;
+                let (start, exclusive_start) = parse_score_boundary_as_float(req.start())?;
+                let (stop, exclusive_stop) = parse_score_boundary_as_float(req.stop())?;
 
                 let order = match req.optional_args().reversed {
                     Some(true) => SortedSetOrder::Descending,
@@ -109,10 +111,7 @@ pub async fn zrange(
             }
         };
 
-        let include_scores = match req.optional_args().with_scores {
-            Some(true) => true,
-            _ => false,
-        };
+        let include_scores = matches!(req.optional_args().with_scores, Some(true));
         match response {
             SortedSetFetchResponse::Hit { value } => {
                 ZRANGE_HIT.increment();
@@ -153,61 +152,4 @@ pub async fn zrange(
         Ok(())
     })
     .await
-}
-
-fn parse_as_integer(value: &[u8]) -> Result<i32, ProxyError> {
-    let index = std::str::from_utf8(value)
-        .map_err(|_| {
-            ProxyError::from(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "ZRANGE index is not valid utf8",
-            ))
-        })?
-        .parse::<i32>()
-        .map_err(|_| {
-            ProxyError::from(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "ZRANGE index is not an integer",
-            ))
-        })?;
-    Ok(index)
-}
-
-// Returns a tuple of (value, is_exclusive)
-fn parse_as_float(value: &[u8]) -> Result<(f64, bool), ProxyError> {
-    // First check if the value is +inf or -inf
-    if value == b"+inf" {
-        return Ok((f64::INFINITY, false));
-    }
-    if value == b"-inf" {
-        return Ok((f64::NEG_INFINITY, false));
-    }
-
-    // Otherwise, split apart '(' and the value if present
-    let (inclusive_symbol, number) = if value[0] == b'(' {
-        (true, &value[1..])
-    } else {
-        (false, value)
-    };
-
-    let score = std::str::from_utf8(number)
-        .map_err(|_| {
-            ProxyError::from(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "ZRANGE score is not valid utf8",
-            ))
-        })?
-        .parse::<f64>()
-        .map_err(|_| {
-            ProxyError::from(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "ZRANGE score is not a float",
-            ))
-        })?;
-
-    if inclusive_symbol {
-        Ok((score, true))
-    } else {
-        Ok((score, false))
-    }
 }
